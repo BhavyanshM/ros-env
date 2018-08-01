1ae29f4688143fb26ddb8945e68b1307b46c2b01
Bhavyansh Mishra <bmishra@ihmc.us>
2018-08-01 18:00:23 -0500

    Fix the commenting for rm

--- Kinetic/install-rtabmap.sh
+++ Kinetic/install-rtabmap.sh
@@ -2,6 +2,4 @@
 
 apt-get update
 apt-get install -y ros-kinetic-rtabmap-ros dbus-x11 libcanberra-gtk3-module
-cd ../../
-git clone https://github.com/introlab/rtabmap_ros.git
-cd rtabmap_ros
+#cd ../../ && rm -rf rtabmap_ros && git clone https://github.com/introlab/rtabmap_ros.git && cd rtabmap_ros
