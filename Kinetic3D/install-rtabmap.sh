269f52e897038a0814ffdbae53fb37345eb3bbf6
Bhavyansh Mishra <bmishra@ihmc.us>
2018-08-14 13:13:54 -0500

    Add some changes to filter parameters and names

--- Kinetic3D/install-rtabmap.sh
+++ Kinetic3D/install-rtabmap.sh
@@ -7,3 +7,4 @@ apt-get install -y ros-kinetic-rtabmap-ros dbus-x11 libcanberra-gtk3-module
 apt-get install -y mesa-utils binutils kmod
 cd ../../nvidia-driver &&  sh NVIDIA-Linux-x86_64-39*.run -a -N --ui=none --no-kernel-module && cd -
 #cd ../../ && rm -rf rtabmap_ros && git clone https://github.com/introlab/rtabmap_ros.git && cd rtabmap_ros
+export ROS_IP=10.100.1.199
